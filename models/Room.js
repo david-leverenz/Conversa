212a269e69cd0ba6e94811ca4e4fccf734990589
Ana Grisham <anaunique@icloud.com>
2023-06-29 08:54:32 -0700

    added code to all model files

--- models/Room.js
+++ models/Room.js
@@ -1,1 +1,29 @@
-//const
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Room extends Model {}
+
+Room.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+       name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },{
+        sequelize,
+        freezeTableName: true,
+        timestamps: true,
+        modelName: 'messages'
+      });
+
+module.exports = Room;
