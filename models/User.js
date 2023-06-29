6912480fc6ac0d5f3dafdce2ba8bd169ddfc8760
Ana Grisham <anaunique@icloud.com>
2023-06-29 08:14:00 -0700

    added user model file

--- models/User.js
+++ models/User.js
@@ -1,1 +1,54 @@
-//const
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8], //setting a min password length
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => { // newUserData is being created and used beacuse of sequelize
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData; //hashing the password so that its not usable
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData; //hashing the password so that its not usable
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true, // testTestTest convert to test_test_test INTERVIEW QUESTION
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
