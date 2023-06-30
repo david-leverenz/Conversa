a872696bf9846fadec62503b7f046f782d0eba39
david-leverenz <david.leverenz@gmail.com>
2023-06-30 11:31:50 -0500

    login button works?

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -17,6 +17,22 @@ router.get('/', async (req, res) => {
     };
 });
 
+router.get('/login', async (req, res) => {
+    try {
+       
+        res.render('login');
+    } catch (error) {
+        res.status(500).json(error)
+    };
+});
 
+router.get('/profile', async (req, res) => {
+    try {
+       
+        res.render('profile');
+    } catch (error) {
+        res.status(500).json(error)
+    };
+});
 
 module.exports = router;
