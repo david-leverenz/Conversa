4ba69269301349521af007cb3e6f38cc4793d4d4
Ana Grisham <anaunique@icloud.com>
2023-06-30 12:41:12 -0700

    fixed all routes to work properly

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,20 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
     console.log(event);
-    const email = document.querySelector('#loginUsername').value.trim();
+    const username = document.querySelector('#loginUsername').value.trim();
     const password = document.querySelector('#loginPassword').value.trim();
 
-    if (email && password) {
+    if (username && password) {
 
         const response = await fetch('/api/login', {
             method: 'POST',
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
 
-            document.location.replace('/profile');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
