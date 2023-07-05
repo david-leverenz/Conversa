9419e9afc0e84d92be8dac328e4cef066ecca816
Ana Grisham <anaunique@icloud.com>
2023-07-05 08:56:17 -0700

    fixed logout event listener

--- controllers/api/logoutRoutes.js
+++ controllers/api/logoutRoutes.js
@@ -1,7 +1,7 @@
 // It was decided to handle user routes in three separate files: "loginRoutes", "logoutRoutes" and "signupRoutes".
-// This is the logout route.  I don't think we need the "const { User }" line because it is not reference in the route.  I will remove this before submitting the final project.
+
 const router = require('express').Router();
-const { User } = require('../../models');
+// const { User } = require('../../models');
 
 router.post('/', (req, res) => {
     if (req.session.logged_in) { 
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,6 @@
 const logout = async () => {
-    const response = await fetch('/api/logoutRoutes', {
+    // alert ('logout');
+    const response = await fetch('/api/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
