1828062fcd011bcafdaa824aa513ae7891f94c2b
Ana Grisham <anaunique@icloud.com>
2023-07-06 08:21:16 -0700

    cleaning code in backend

--- controllers/api/session.js
+++ controllers/api/session.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const {Messages, User, Room} = require('../../models')
 
 router.get('/', async (req, res) => {
     try {
         const sessions = req.session.userName;
         // Respond with the fetched sessions
-        // console.log("req.session");
-        // console.log(req.session);
         res.json(sessions);
     } catch (error) {
         // Handle any errors that occurred during the database operation
--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -12,19 +12,13 @@ router.get('/', async (req, res) => {
             }]
         });
         const newRoomData = roomData.map(room => room.get ({ plain:true }));
-        //console.log(newRoomData);
+
         res.render('login', {newRoomData});
     } catch (error) {
         res.status(500).json(error)
     };
 });
-// router.get('/',(req,res)=>{
-//     res.redirect('/chat')
-// })
-// router.get('/chat', (req, res) => {
-//     res.render('chat')
-// })
-// // com
+
 router.get('/chat', async (req, res) => {
     try {
 
@@ -41,30 +35,5 @@ router.get('/signup', async (req,res) => {
         res.status(500).json(error);
     }
 })
-
-// router.get('/profile', async (req, res) => {
-//     try {
-
-//         res.render('profile');
-//     } catch (error) {
-//         res.status(500).json(error)
-//     };
-// });
-
-// router.get('/sessions', async (req, res) => {
-//     try {
-//         const sessions = req.session.userName;
-//         // Respond with the fetched sessions
-//         res.json(sessions);
-//     } catch (error) {
-//         // Handle any errors that occurred during the database operation
-//         console.error('Error retrieving sessions:', error);
-//         res.status(500).json({ error: 'Failed to retrieve sessions' });
-//     }
-// });
-
-
-
-
 
 module.exports = router;
